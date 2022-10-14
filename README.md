f53d40d7ac80e32f94423a5eef2a655d6efdbb6b
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-10-14 17:19:21 -0400

    Add Mock API

--- README.md
+++ README.md
@@ -29,42 +29,26 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+# Mock API Implementation
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+```bash
+npm install -g json-server
+cd test && json-server db.js --port 4000
+```
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Reference: [SwaggerHub](https://app.swaggerhub.com/apis/swagger292/Toktik/1.0.0)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Implemented API:
++ /signup
++ /login
++ /user/userId
++ /post or /activity
++ /post/postId
++ /comment/commentId
++ /following
++ /follower
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Notes:
++ Comment: You need to update both /comment and /post/postId
++ Like: You need to update /post/postId
++ Follow: You need to update /following and /follower and /user/userId
---
+++ test/db.js
@@ -0,0 +1,152 @@
+const faker = require('@faker-js/faker').faker;
+const fs = require('fs');
+const images = fs.readFileSync('image_urls.txt', 'utf-8').split(/\r?\n/);
+const comments = fs.readFileSync('comments.txt', 'utf-8').split(/\r?\n/);
+
+const allUsersNum = 15;
+const perComment = 5;
+
+const getAllUsernames = (allUsersNum) => {
+    let usernames = ["demo"];
+    for (let i = 0; i < allUsersNum-1; i++) {
+        usernames.push(faker.internet.userName());
+    }
+    return usernames;
+}
+
+const allUsernames = getAllUsernames(allUsersNum);
+
+let globalPostId = 100;
+let globalCommentId = 100;
+
+const allPosts = [];
+const allComments = [];
+const followingMap = {};
+const follwerMap = {};
+
+const generateFollowingRelationship = () => {
+    for (let i = 1; i <= allUsersNum; i++) {
+        for (let j = 1; j <= allUsersNum; j++) {
+            if (i === j) continue;
+            if (followingMap[i] === undefined) {
+                followingMap[i] = [];
+            }
+            if (follwerMap[j] === undefined) {
+                follwerMap[j] = [];
+            }
+
+            if (Math.random() > 0.5) {
+                followingMap[i].push(j);
+                follwerMap[j].push(i);
+            }
+        }
+    }
+}
+
+const getRandomArray = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+const getComment = (postUsername, postId) => {
+    let comment = {
+        id: globalCommentId++,
+        username: getRandomArray(allUsernames),
+        postId: postId,
+        message: getRandomArray(comments),
+        mention: postUsername,
+    };
+    allComments.push(comment);
+    return comment;
+}
+
+const getMultipleComments = (postUsername, postId, n) => {
+    let comments = [];
+    for (let i = 0; i < n; i++) {
+        comments.push(getComment(postUsername, postId));
+    }
+    return comments;
+}
+
+const getPost = (username) => {
+    let post = {
+        id: globalPostId,
+        username: username,
+        postType: 0,
+        postContent: getRandomArray(images),
+        description: faker.lorem.sentence(),
+        public: true,
+        totalLikes: Math.floor(Math.random() * allUsersNum),
+        tagging: [],
+        comments: getMultipleComments(username, globalPostId, perComment),
+    };
+
+    allPosts.push(post);
+    globalPostId++;
+    return post;
+}
+
+const getMultiplePosts = (username, n) => {
+    let posts = [];
+    for (let i = 0; i < n; i++) {
+        posts.push(getPost(username));
+    }
+    return posts;
+}
+
+const getUser = (n) => {
+    let users = [
+        {
+            id: 1,
+            username: "demo",
+            firstName: "Jack",
+            lastName: "J",
+            email: "abc@gmail.com",
+            password: "123456",
+            profilePicture: "https://ui-avatars.com/api/?rounded=true",
+            followerCount: follwerMap[1].length,
+            followingCount: followingMap[1].length,
+            postCount: 22,
+            posts: getMultiplePosts("demo", 22),
+        }
+    ]
+
+    for (let i = 0; i < n-1; i++) {
+        let tmpUsername = allUsernames[i+1];
+        let postNum = Math.floor(Math.random() * 10) + 5;
+
+        users.push({
+            id: i+2,
+            username: tmpUsername,
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            profilePicture: "https://ui-avatars.com/api/?rounded=true",
+            followerCount: follwerMap[i+2].length,
+            followingCount: followingMap[i+2].length,
+            postCount: postNum,
+            posts: getMultiplePosts(tmpUsername, postNum),
+        })
+    }
+
+    return users;
+}
+
+
+
+module.exports = () => {
+    generateFollowingRelationship();
+
+    const data = {
+        signup: { success: true },
+        login: { success: true },
+        user: getUser(allUsersNum),
+        post: allPosts,
+        activity: allPosts,
+        comment: allComments,
+        following: followingMap,
+        follower: follwerMap,
+    }
+
+    return data
+}
