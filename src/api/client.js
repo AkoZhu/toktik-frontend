a79b0aff3f2c609568781a528f21494ea54c9ed6
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-12-06 15:19:58 -0500

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const apiEndpoint = "http://localhost:4000/api";
+export const apiEndpoint = process.env.API_ENDPOINT || "http://localhost:4000/api";
 
 export const client = axios.create({
     baseURL: apiEndpoint,
